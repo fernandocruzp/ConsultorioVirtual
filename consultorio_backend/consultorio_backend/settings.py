189e5a73e5677ec8ac24208eedd6b10e050225d7
fernandocruzp <cruzfernando@ciencias.unam.mx>
2025-06-23 17:51:01 -0600

    Agregar allowed 2

--- consultorio_backend/consultorio_backend/settings.py
+++ consultorio_backend/consultorio_backend/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://pinedaintegralmedic.onrender.com/"]
+ALLOWED_HOSTS = ["pinedaintegralmedic.onrender.com/"]
 
 
 # Application definition
