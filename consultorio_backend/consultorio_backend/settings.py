564e39e2b38bbba55e644f6aacb05ddb0c34fa6a
fernandocruzp <112268917+fernandocruzp@users.noreply.github.com>
2025-06-24 13:43:28 -0600

    Corregir correo 5

--- consultorio_backend/consultorio_backend/settings.py
+++ consultorio_backend/consultorio_backend/settings.py
@@ -170,4 +170,4 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS') == 'True'
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
